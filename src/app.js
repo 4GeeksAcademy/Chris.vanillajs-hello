ab5ae6093368e092264d179e53bcdef83c61ebf5
ChrisKeith01 <165314148+ChrisKeith01@users.noreply.github.com>
2024-05-20 16:20:47 +0000

    CompletedProject

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["The", "Our", "My", "this", "Your"];
+  let adj = ["Big", "Creative", "Helpful", "Fast", "Strong"];
+  let noun = ["Develpoers", "Engineers", "Analyst", "Project", "Agent"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        let div = document.createElement(`div`);
+        div.innerText = `
+        ${pronoun[i]}${adj[a]}${noun[n]}.com
+        ${pronoun[i]}${adj[a]}${noun[n]}.net
+        ${pronoun[i]}${adj[a]}${noun[n]}.org
+        ${pronoun[i]}${adj[a]}${noun[n]}.edu
+        `;
+        document.body.appendChild(div);
+      }
+    }
+  }
 };
